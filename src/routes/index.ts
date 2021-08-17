d20bfda738447a303d980b7da6cea79247fe14d1
drDre-code <ebeneezer.akinkahunsi@decagon.dev>
2021-08-17 01:38:40 +0100

    additional correction

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ router.get('/', (req: express.Request, res: express.Response) => {
   res.send(`
   <h1> DRE-TRANSACTION-API</h1>
   <div>information: use "/accounts" to view all accounts in database</div>
-  <div>information: use "/acounts/{accountNumber}" to view specific account in database</div>
+  <div>information: use "/accounts/{accountNumber}" to view specific account in database</div>
   <div>information: use "/transer" to view all transactions in database</div>
   <div>information: use "/transer/{reference}" to view specific transaction in database</div>
   <div><a href=https://documenter.getpostman.com/view/16998071/Tzz7QJcE>documentationLink</a></div>
